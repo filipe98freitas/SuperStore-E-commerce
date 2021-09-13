ddda4b9bf76965298cf83805da32b135f7c9365e
Caio Nogueira <nogueira998@gmail.com>
2021-09-14 00:50:03 +0200

    Adição de lista de categorias no Form

--- src/components/ManagerProducts.js
+++ src/components/ManagerProducts.js
@@ -2,9 +2,11 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 
+
 class ManagerProducts extends React.Component {
   state = {
     products: [],
+    categories:[]
   };
 
   componentDidMount = async () => {
@@ -12,14 +14,21 @@ class ManagerProducts extends React.Component {
       const response = await axios.get(
         "https://ironrest.herokuapp.com/caiofilipeSuperstore"
       );
-      this.setState({ products: [...response.data] });
+      this.setState({ products: [...response.data]});
       console.log(this.state);
     } catch (err) {
       console.error(err);
     }
   };
 
+
+
   render() {
+
+    //Remover duplicidade da categoria
+    const categoryArr = this.state.products.map((obj) => obj.category).sort()
+    const newCategoryArr = categoryArr.filter((el, index) => categoryArr.indexOf(el) === index)
+
     return (
       <div className="container">
         <div className="row">
@@ -107,10 +116,7 @@ class ManagerProducts extends React.Component {
                   aria-describedby="validationServer04Feedback"
                   required
                 >
-                  <option selected disabled value="">
-                    Choose...
-                  </option>
-                  <option>...</option>
+                {newCategoryArr.map((obj) => <option>{obj}</option>)}
                 </select>
                 <div
                   id="validationServer04Feedback"
@@ -145,7 +151,25 @@ class ManagerProducts extends React.Component {
               </div>
             </form>
           </div>
-          <div className="col-6"></div>
+          {/* Lista de produtos */}
+          <div className="col-6">
+            <div className="checkoutProduct">
+              <img
+                className="checkoutProduct__image"
+                src="https://www.dinamize.com.br/wp-content/uploads/2020/07/4-Ps-do-marketing-produto.png"
+                alt="imagem do produto"
+              />
+              <div className="checkoutProduct__info">
+                <p className="checkoutProduct__title">Produto X</p>
+                <p className="checkoutProduct__price">
+                  <small>$</small>
+                  <strong>000</strong>
+                </p>
+                <div className="checkoutProduct__rating"></div>
+                <button>Remove from Basket</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
