60996f901406a211ad14ae7a8b24d800483ed5d6
filipe98freitas <filipe98freitas@gmail.com>
2021-09-13 20:31:38 +0100

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "../css/Home.css";
+import topH from "../images/topHome.jpg";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home_container">
+        <img className="home_image" src={topH} alt="imagem de fundo" />
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+        </div>
+
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+
+        <div className="home__row">{/* Product */}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
