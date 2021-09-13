90ab70d1ae076c17106656ca682b784ec369f95a
filipe98freitas <filipe98freitas@gmail.com>
2021-09-13 15:39:12 +0100

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA2oQfHj8ZkRIKQqF8x2XzkLxUBqUrSoyk",
+  authDomain: "e-commerce-8df12.firebaseapp.com",
+  projectId: "e-commerce-8df12",
+  storageBucket: "e-commerce-8df12.appspot.com",
+  messagingSenderId: "163848518411",
+  appId: "1:163848518411:web:9c50c8712d1023a9f1000e",
+  measurementId: "G-W7BVF3F655",
+};
