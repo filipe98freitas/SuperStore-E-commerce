a97ab3a1901cfabfbe20057f94138647392fe5e1
Caio Nogueira <nogueira998@gmail.com>
2021-09-13 16:12:59 +0200

    adição de banner na pagina de Checkout

---
+++ src/components/Checkout.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../css/Checkout.css";
+import bannerImg from "../images/bannerCheckout.jpg"
+
+
+function Checkout() {
+;
+
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src={bannerImg}
+          alt=""
+        />
+
+        <div>
+          <h3>Hello, Usuário</h3>
+          <h2 className="checkout__title">Your shopping Basket</h2>
+
+        </div>
+      </div>
+
+      <div className="checkout__right">
+
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
