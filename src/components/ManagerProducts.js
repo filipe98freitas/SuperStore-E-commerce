22c137b61d0690601e26cf1d981db16a12babaa6
Caio Nogueira <nogueira998@gmail.com>
2021-09-14 01:45:35 +0200

    Fiz o formulário de produto, falta os eventos

--- src/components/ManagerProducts.js
+++ src/components/ManagerProducts.js
@@ -2,11 +2,10 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 
-
 class ManagerProducts extends React.Component {
   state = {
     products: [],
-    categories:[]
+    categories: [],
   };
 
   componentDidMount = async () => {
@@ -14,29 +13,28 @@ class ManagerProducts extends React.Component {
       const response = await axios.get(
         "https://ironrest.herokuapp.com/caiofilipeSuperstore"
       );
-      this.setState({ products: [...response.data]});
+      this.setState({ products: [...response.data] });
       console.log(this.state);
     } catch (err) {
       console.error(err);
     }
   };
 
-
-
   render() {
-
     //Remover duplicidade da categoria
-    const categoryArr = this.state.products.map((obj) => obj.category).sort()
-    const newCategoryArr = categoryArr.filter((el, index) => categoryArr.indexOf(el) === index)
+    const categoryArr = this.state.products.map((obj) => obj.category).sort();
+    const newCategoryArr = categoryArr.filter(
+      (el, index) => categoryArr.indexOf(el) === index
+    );
 
     return (
       <div className="container">
         <div className="row">
-          <div className="col-6">
+          <div className="col-6 mt-5">
             <form className="row g-3">
-              <div className="col-12">
+              <div className="col-10">
                 <label htmlFor="productName" className="form-label">
-                  Product Name
+                  <strong>Product Name</strong>
                 </label>
                 <input
                   type="text"
@@ -45,12 +43,12 @@ class ManagerProducts extends React.Component {
                   required
                 />
                 <div id="validationName" className="valid-feedback">
-                  Ok!
+                  Ok
                 </div>
               </div>
-              <div className="col-12">
+              <div className="col-10">
                 <label htmlFor="productDescription" className="form-label">
-                  Description
+                  <strong>Description</strong>
                 </label>
                 <input
                   type="text"
@@ -59,12 +57,12 @@ class ManagerProducts extends React.Component {
                   required
                 />
                 <div id="validationDescription" className="valid-feedback">
-                  Looks good!
+                  Ok
                 </div>
               </div>
-              <div className="col-12">
+              <div className="col-10">
                 <label htmlFor="productImage" className="form-label">
-                  Image Url
+                  <strong>Image Url</strong>
                 </label>
                 <input
                   type="text"
@@ -77,15 +75,15 @@ class ManagerProducts extends React.Component {
                   id="validationServer03Feedback"
                   className="invalid-feedback"
                 >
-                  Please provide a valid city.
+                  Please provide a image Url.
                 </div>
               </div>
-              <div className="col-md-4">
+              <div className="col-4">
                 <label
                   htmlFor="validationServerUsername"
                   className="form-label"
                 >
-                  Price
+                  <strong>Price</strong>
                 </label>
                 <div className="input-group has-validation">
                   <span className="input-group-text" id="inputGroupPrepend3">
@@ -102,32 +100,28 @@ class ManagerProducts extends React.Component {
                     id="validationServerUsernameFeedback"
                     className="invalid-feedback"
                   >
-                    Please choose a username.
+                    Please provide a price.
                   </div>
                 </div>
               </div>
-              <div className="col-4">
+              <div className="col-3">
                 <label htmlFor="validationServer04" className="form-label">
-                  Category
+                  <strong>Category</strong>
                 </label>
                 <select
-                  className="form-select is-invalid"
+                  className="form-select"
                   id="validationServer04"
                   aria-describedby="validationServer04Feedback"
                   required
                 >
-                {newCategoryArr.map((obj) => <option>{obj}</option>)}
+                  {newCategoryArr.map((obj) => (
+                    <option>{obj}</option>
+                  ))}
                 </select>
-                <div
-                  id="validationServer04Feedback"
-                  className="invalid-feedback"
-                >
-                  Please select a valid state.
-                </div>
               </div>
-              <div className="col-4">
+              <div className="col-3">
                 <label htmlFor="validationServer05" className="form-label">
-                  Rating
+                  <strong>Rating</strong>
                 </label>
                 <input
                   type="number"
@@ -140,7 +134,7 @@ class ManagerProducts extends React.Component {
                   id="validationServer05Feedback"
                   className="invalid-feedback"
                 >
-                  Please provide a valid zip.
+                  Please provide a rating.
                 </div>
               </div>
 
@@ -152,22 +146,34 @@ class ManagerProducts extends React.Component {
             </form>
           </div>
           {/* Lista de produtos */}
-          <div className="col-6">
-            <div className="checkoutProduct">
-              <img
-                className="checkoutProduct__image"
-                src="https://www.dinamize.com.br/wp-content/uploads/2020/07/4-Ps-do-marketing-produto.png"
-                alt="imagem do produto"
-              />
-              <div className="checkoutProduct__info">
-                <p className="checkoutProduct__title">Produto X</p>
-                <p className="checkoutProduct__price">
-                  <small>$</small>
-                  <strong>000</strong>
-                </p>
-                <div className="checkoutProduct__rating"></div>
-                <button>Remove from Basket</button>
-              </div>
+          <div className="col-6 mt-3">
+            <h2>Product List</h2>
+            <div
+              className="list-group bg-white"
+              style={{ maxHeight: "85vh", overflow: "scroll" }}
+            >
+              {this.state.products.map((productObj) => {
+                return (
+                  <div className="checkoutProduct">
+                    <img
+                      className="checkoutProduct__image"
+                      src={productObj.image}
+                      alt="imagem do produto"
+                    />
+                    <div className="checkoutProduct__info">
+                      <p className="checkoutProduct__title">
+                        {productObj.title}
+                      </p>
+                      <p className="checkoutProduct__price">
+                        <small>$</small>
+                        <strong>{productObj.price}</strong>
+                      </p>
+                      <div className="checkoutProduct__rating"></div>
+                      <button>Remove</button>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
