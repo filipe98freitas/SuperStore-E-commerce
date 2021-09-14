2c80ff4009dc02aa06d480c1c7a2521645bd0b48
filipe98freitas <filipe98freitas@gmail.com>
2021-09-14 17:10:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Checkout from "./components/Checkout";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import ManagerProducts from "./components/ManagerProducts"
+import ManagerProducts from "./components/ManagerProducts";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Switch>
-        <Route exact path="/restricted" component={ManagerProducts}/>
+        <Route exact path="/restricted" component={ManagerProducts} />
         <Route exact path="/checkout" component={Checkout} />
         <Route path="/" component={Home} />
       </Switch>
+      <Footer />
     </BrowserRouter>
   );
 }
