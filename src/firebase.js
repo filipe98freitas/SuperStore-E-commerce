25de0203abd75d8d757d329bacaf8d32e7b78eaf
filipe98freitas <filipe98freitas@gmail.com>
2021-09-14 23:03:19 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyA2oQfHj8ZkRIKQqF8x2XzkLxUBqUrSoyk",
   authDomain: "e-commerce-8df12.firebaseapp.com",
@@ -8,3 +9,10 @@ const firebaseConfig = {
   appId: "1:163848518411:web:9c50c8712d1023a9f1000e",
   measurementId: "G-W7BVF3F655",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
